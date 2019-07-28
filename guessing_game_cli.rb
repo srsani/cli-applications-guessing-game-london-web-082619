d503b88acc195092ac83d90d5713031793842f3b
droll-accelerator-1483 <srsani@gmail.com>
2019-07-28 19:49:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+def run_guessing_game()
+  # r = Random.new
+  guess_number = rand(6)
+  p guess_number
+  p "guess the a number between 1 and 6"
+  user_input = gets.chomp
+  if user_input.to_i == guess_number
+    p "You guessed the correct number!"
+  elsif user_input.to_i != guess_number
+    p "Sorry! The computer guessed #{guess_number}."
+  elsif user_input == "exit"
+    p "Goodbye!"
+  end
+end
+run_guessing_game
